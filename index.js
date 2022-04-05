a36fc47ae7c942136eebc02d9a42750150596092
Justin Li <justinlit23@gmail.com>
2022-04-05 16:25:34 -0700

    commit tree 629c365ee0665cd5650ab561df897cf90102b68a

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const drivers = ['Alex', 'Brian', 'Carl'];
+const returnFirstTwoDrivers = (drivers) => {
+    return drivers.slice(0,2);  
+}
+
+const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (multiplier) => {
+    const fareMultiplier = (fare) => multiplier * fare;
+    return fareMultiplier;
+}
+
+const fareDoubler = (fare) => createFareMultiplier(2)(fare);
+
+const fareTripler = (fare) => createFareMultiplier(3)(fare);
+
+const selectDifferentDrivers = (drivers, selectedDrivers) => {
+    if (selectedDrivers === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(drivers)
+    } else if (selectedDrivers === returnLastTwoDrivers) {
+        return returnLastTwoDrivers(drivers)
+    }
+}
